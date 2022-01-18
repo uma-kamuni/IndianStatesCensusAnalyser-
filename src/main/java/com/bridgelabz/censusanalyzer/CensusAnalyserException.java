f3793be9f59665bb6820cc8795ff8aaea2033081
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-17 22:09:09 -0800

    Added: ability for analyser to load indian census information

---
+++ src/main/java/com/bridgelabz/censusanalyzer/CensusAnalyserException.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.censusanalyzer;
+
+
+public class CensusAnalyserException extends Exception {
+
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
--- src/main/java/com/bridgelabz/censusanalyzer/CensusAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.censusanalyzer;
-
-public class CensusAnalyzer {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Indian States Census Analyser Problem");
-	}
-
-}
