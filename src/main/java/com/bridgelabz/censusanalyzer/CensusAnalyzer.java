6ed0f067c0e4ab5b605286ed7a2d2f2eec07e831
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-17 20:36:47 -0800

    Welcome to Indian States Census Analyser Problem

---
+++ src/main/java/com/bridgelabz/censusanalyzer/CensusAnalyzer.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.censusanalyzer;
+
+public class CensusAnalyzer {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyser Problem");
+	}
+
+}
